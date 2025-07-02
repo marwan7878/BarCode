adedeba7b2923b9a9d9b0439e7c2dfab6cbffa71
marwan <marwanmohamed7887@gmail.com>
2025-07-02 14:24:15 +0300

    configure project files and packages to start in task

---
+++ BarCode/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BarCode.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
